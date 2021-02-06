7663ee1a0af33517bc3978be48170e7a922b0a04
Sebastien <sebastienbn8@gmail.com>
2021-02-06 17:06:59 -0500

    Added router and home page

---
+++ stay-green/src/Components/Router.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./HomePage/HomePage";
+
+
+export default function RouterPage() {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route path='/' exact>
+                        <HomePage />
+                    </Route>
+                    <Route path='/like' exact>
+                        <LikePage />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+}

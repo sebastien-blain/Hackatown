0afc9c9ff31f633c88299c46fbdf8946bdc11cb9
Sebastien <sebastienbn8@gmail.com>
2021-02-06 17:42:33 -0500

    no changes

--- stay-green/src/Components/GiftComponent.js
+++ stay-green/src/Components/GiftComponent.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
 
-//Interest event tracking
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+
+}));
+
 export default function GiftComponent(props) {
+
+    const classes = useStyles();
 
     return (
         <div>
             <h1>Hello</h1>
-            <Card>
+            <Card className={classes.root}>
                 <CardMedia
                     image={props.link}
                     title={props.name}
                 />
-
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {props.Description}
+                    </Typography>
+                </CardContent>
             </Card>
         </div>
     );
